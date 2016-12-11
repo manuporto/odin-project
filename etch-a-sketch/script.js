93554d3c69f37935cec2ea5ef81075fc0c6c9c28
manuporto <manu.porto94@gmail.com>
2016-12-11 13:12:23 -0300

    Add etch-a-sketch project

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,43 @@
+/*jslint browser: true plusplus: true */
+/*global $, jQuery, alert*/
+
+function createGrid(size) {
+    //"use strict";
+    var i, gwidth = $('#grid').css('width'),
+        blockSize = 600 / Math.sqrt(size),
+        styles = {
+            'width': blockSize,
+            'height': blockSize
+        };
+    
+    for (i = 0; i < size; i++) {
+        $('#grid').append("<div class='block'></div>");
+    }
+    $('.block').css(styles);
+}
+
+function draw() {
+    $('.block').hover(function () {
+        $(this).css('background-color', 'black');
+    });
+}
+
+$(document).ready(function () {
+    //"use strict";
+    var MAX_SIZE = 30,
+        MIN_SIZE = 5,
+        gsize = MIN_SIZE;
+    createGrid(Math.pow(gsize, 2));
+    
+    draw();
+   
+    $('#new').click(function () {
+        gsize = parseInt(prompt("New grid size (blocks per row): ", 5));
+        if (gsize > MAX_SIZE || gsize < MIN_SIZE) {
+           gsize = MIN_SIZE; 
+        }
+        $('#grid').html('');
+        createGrid(Math.pow(gsize, 2));
+        draw();
+    });
+});
